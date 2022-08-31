4e223c8e7b76d839b9613a6b2b4a97db3a898f5c
Josue Arce <jska2.1@gmail.com>
2022-08-31 17:49:01 -0400

    Add class domicilio and foreign Key in class empleado, add migrations for creation class domicilio in database

--- empleados/admin.py
+++ empleados/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from empleados.models import Empleado
+from empleados.models import Empleado, Domicilio
 
 admin.site.register(Empleado)
+admin.site.register(Domicilio)
--- empleados/models.py
+++ empleados/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Domicilio(models.Model):
+    calle = models.CharField(max_length=255)
+    no_calle = models.IntegerField()
+    region = models.CharField(max_length=255)
+    comuna = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Domicilio : {self.calle} {self.no_calle}, {self.comuna} Region {self.region} '
 
 class Empleado(models.Model):
     rut = models.CharField(max_length=255)
@@ -10,6 +18,7 @@ class Empleado(models.Model):
     sexo = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     telefono = models.CharField(max_length=255)
+    domicilio = models.ForeignKey(Domicilio, on_delete=models.SET_NULL, null = True)
 
     def __str__(self):
         return f'Empleado {self.id}: Rut {self.rut}, Nombre {self.nombre} {self.apellido} '
