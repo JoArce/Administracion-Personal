db1117bedf108a7d7c8b0614e9c9e9648d2ec738
Josue Arce <jska2.1@gmail.com>
2022-09-01 19:49:06 -0400

    Create template bienvenido.html, add no_personas in views

--- webapp/views.py
+++ webapp/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from empleados.models import Empleado, Domicilio
+
+
 def bienvenido(request):
-     return HttpResponse('Hola mundo')
+     no_personas = Empleado.objects.count()
+     return render(request, 'bienvenida.html', {'no_personas':no_personas})
